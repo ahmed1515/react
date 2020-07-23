dc3bef2672fc07620cf06231c27f00a3cbc2ae22
Muhammad Ahmad Raza <ahmed.sharif@zameen.com>
2020-07-23 12:41:40 +0500

    Creating and manipulating components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
+/*
+ * Always use first Capital letter for the component name
+*/
+
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom' 
 
+// Create component by function 
 
-/* Using dynamic elements*/
-var name = 'Umer'
-var text = <div><h1 class="header-section">Welcome {name}</h1><h2> You will be enjoyed here </h2></div>
+// let User = function(props){
+// 	return <h1>Hello {props.name}</h1>
+// };
 
-/* Using object */
-let user = {
-	firstname : 'M Ahmad',
-	lastname : 'Raza'
+// Create component by class
+
+class User extends React.Component{
+	render(){
+		return <h1>Hello {this.props.name} your team is {this.props.team}</h1>
+	}
 }
-// <h1> Welcome { user.firstname} {user.lastname} </h1>,
 
-// Using functions
-let cartoon = function(name,show) {
-	return <h1> Cartoon name is {name} and its show time is {show} </h1>
+function Show(){
+	return <div>	
+				<User name = "Ahmad Raza" team = "Bayut"/>
+				<User name = "Hamza" team = "CRM"/>
+			</div>
 }
 
 ReactDOM.render(
-	cartoon('doremon','9-11 AM'),
+	<Show/>,
 	document.getElementById('root')
 );
